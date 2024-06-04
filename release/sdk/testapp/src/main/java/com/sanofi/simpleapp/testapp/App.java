87e6ea2e3c7f450304ea1ed5e3380c5d7ce297d3
srinivash-balachandran <srinivash.balachandran@sanofi.com>
2024-06-04 17:26:25 -0400

    app code comit

---
+++ release/sdk/testapp/src/main/java/com/sanofi/simpleapp/testapp/App.java
@@ -0,0 +1,13 @@
+package com.sanofi.simpleapp.testapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
